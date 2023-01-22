1dd050a178bfd9c6a6605785ffdd9bd32988e5cc
Tosi <rodrigotosi10@gmail.com>
2023-01-21 21:03:14 -0300

    Project created

---
+++ backend/src/test/java/com/tosi/daion/DaionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tosi.daion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
