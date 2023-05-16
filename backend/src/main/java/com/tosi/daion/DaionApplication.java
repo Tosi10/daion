11ae076c337c6049929615f63d4d7634d0490e47
Rodrigo Tosi <rodrigotosi10@gmail.com>
2023-05-16 17:17:52 -0300

    Projeto criado

---
+++ backend/src/main/java/com/tosi/daion/DaionApplication.java
@@ -0,0 +1,13 @@
+package com.tosi.daion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaionApplication.class, args);
+	}
+
+}
